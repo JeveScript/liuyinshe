6c8d90084eade4b806e4f78e0082800f50cfd950
涛 <tao@taodeMacBook-Pro.local>
2019-09-25 17:36:55 +0800

    fix qiniu.js

--- controllers/qiniu.js
+++ controllers/qiniu.js
@@ -3,17 +3,20 @@ var config = require('./../config');
 var accessKey = config.qiniu.AccessKey;
 var secretKey = config.qiniu.SecretKey
 var domain = config.qiniu.domain
-var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
 
-var options = {
-    scope: 'liuyinshe',
-  };
-var putPolicy = new qiniu.rs.PutPolicy(options);
-var uploadToken=putPolicy.uploadToken(mac);
+
+
 
 const qiniuController = {
     setQiniuToken:function(req, res,next){
         try{
+            var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+            var options = {
+                scope: 'liuyinshe',
+            };
+            var putPolicy = new qiniu.rs.PutPolicy(options);
+            var uploadToken=putPolicy.uploadToken(mac);
+
             res.json({code:200, data:{uploadToken,domain}, messsage:'请求成功'})
         }catch{
             res.json({code:0, messsage:"服务器错误"})
